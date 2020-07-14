a9e013c0d675d69aed7c8a30f1bcba37cdf98dd3
qianbing <1532498760@qq.com>
2020-07-15 07:59:36 +0800

    four

---
+++ src/views/CourseDetail/Component/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <header>
+            <div class="header">
+                <span @click="$router.back(-1)"><i class="cubeic-back"></i></span>
+                <div class="title">
+                    {{ videoInfo.title }}
+                </div>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        props:{
+            videoInfo:{
+                required:true,
+                type:Object
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .header {
+        display: flex;
+        background-color: darkblue;
+        padding:10px 20px;
+        color:#fff;
+    }
+    .cubeic-back{
+        color:#fff;
+        margin-right: 5px;
+    }
+    .title{
+        flex: 1;
+        font-size: 16px;
+        //超出省略
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+</style>
---
+++ src/views/CourseDetail/Component/Tab.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <cube-tab-bar
+                v-model="selectedLabelSlots"
+                show-slider
+        >
+            <cube-tab v-for="(item, index) in tabs" :label="item.label" :key="item.label">
+            </cube-tab>
+        </cube-tab-bar>
+        <component :videoInfo="videoInfo" :videoDetailInfo="videoDetailInfo" :is='selectedLabelSlots == "简介"?"Summary":"CataLog"'></component>
+    </div>
+</template>
+
+<script>
+    import Summary from './Summary'
+    import CataLog from './CataLog'
+    export default {
+        name: "Tab",
+        props:{
+          videoInfo:{
+              type: Object,
+              required: true
+          },
+          videoDetailInfo:{
+              type: Array,
+              required: true
+          }
+        },
+        data () {
+            return {
+                selectedLabelSlots: '简介',
+                tabs: [{
+                    label: '简介',
+                }, {
+                    label: '目录',
+                }]
+            }
+        },
+        components:{
+            Summary,
+            CataLog
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
